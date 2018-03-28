80ac1ffdd9b27a200f17a9e669f22b78bd3509d3
Anderson Pessoa <anderson.pessoa002@gmail.com>
2018-03-28 16:42:32 -0300

    tudo ok...

--- src/estructure/block.ts
+++ src/estructure/block.ts
@@ -1,3 +1,51 @@
-var blockBase = {
+import * as crypto from "crypto";
 
+export default class Block<T> {
+	blockHeigth: number;
+	timestamp: number;
+	difficulty: number;
+	nonce: number;
+	data: T;
+	hash: string;
+	previousBlock: Block<T>;
+
+	constructor(data: T, previousBlock: Block<T> = null ,difficulty: number = 1) {
+		this.data = data;
+		this.timestamp = Date.now();
+		this.difficulty = difficulty;
+		this.nonce = 0;
+		this.previousBlock = previousBlock;
+
+		this.mine();
+	}
+
+	private generateSha256() {
+		console.log(this);
+		return crypto.createHmac("sha256","blockchain").digest("hex");
+	}
+
+	private mine() {
+		this.hash = this.generateSha256();
+
+		while (!this.hash.match("^(0){" + this.difficulty + "}")) {
+			this.nonce++;
+			this.hash = this.generateSha256();
+		}
+	}
+
+	public toString(){
+		return JSON.stringify({
+			blockHeigth: this.blockHeigth,
+			timestamp : this.timestamp,
+			difficulty : this.difficulty,
+			data : this.data,
+			nonce : this.nonce,
+			hash : this.hash,
+			previousHash : this.previousBlock.hash
+		})
+	}
+
+	public printBlock(){
+		console.log(this.toString())
+	}
 }
--- src/estructure/blockChain.ts
+++ src/estructure/blockChain.ts
@@ -1,4 +1,39 @@
-import shar256 from "sha256";
+import Block from "./block"
 
-var difficulty;
-var maxNounce = Math.pow(2,1024-1);
+export default class BlockChain<T> {
+
+	blocks : Block<T>[];
+
+	constructor(data : T, difficult : number = 1) {
+		this.blocks = [];
+		this.addBlock(data, difficult);
+	}
+
+	public getGenesisBlock(){
+		return this.blocks[0];
+	}
+
+	public getLastBlock(){
+		if(this.blocks.length > 0) return this.blocks[this.blocks.length - 1];
+
+		else if (this.blocks.length == 0) return this.blocks[this.blocks.length];
+
+		else return null;
+	}
+	
+	public addBlock(data : T, difficult : number){
+		var last = this.getLastBlock();
+
+		var newBlock = new Block<T>(data, last, difficult);
+		
+		newBlock.blockHeigth = last.blockHeigth ? last.blockHeigth + 1 : 0;
+
+		this.blocks.push(newBlock);
+	}
+
+	public printChain(){
+		this.blocks.forEach(block => {
+			block.printBlock();
+		});
+	}
+}
